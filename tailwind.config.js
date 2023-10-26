3d611cc522df616a014a9ad9d153382f89296398
Abdulvahobjon <abdulvahob0714@gmail.com>
2023-10-26 21:56:46 +0500

    commid

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        "service-gradiend":"linear-gradient(180deg, #252527 30.75%, #0186EF00 615.25%)",
+        "service-bg":"linear-gradient(0deg, #141415 0%, #141415 100%), #FFF"
+      },
+    },
   },
   plugins: [],
 }
-
