832064494a35634036a2872572eedbda35c0406f
Abdulvahobjon <abdulvahob0714@gmail.com>
2023-11-02 13:04:38 +0500

    hamkorlar section end

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,18 @@ let circleText  = document.querySelector("#circle-text")
 circleText.innerHTML = circleText.innerText.split("").map((item , index)=>{
   return  `<span style="transform:rotate(${index*8.2}deg)">${item}</span>`
 }).join("")
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 6,
+  spaceBetween: 21,
+  loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
