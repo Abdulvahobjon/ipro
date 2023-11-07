64e050001e166e71f9ce0b37750cbd98012f71e8
Abdulvahobjon <abdulvahob0714@gmail.com>
2023-11-07 08:46:39 +0500

    commid

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,3 @@
-let circleText  = document.querySelector("#circle-text")
-circleText.innerHTML = circleText.innerText.split("").map((item , index)=>{
-  return  `<span style="transform:rotate(${index*8.2}deg)">${item}</span>`
-}).join("")
-
-
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 6,
   spaceBetween: 21,
@@ -17,3 +11,13 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+function textRounded() {
+  let circleText = document.querySelector("#circle-text");
+  circleText.innerHTML = circleText.innerText
+    .split("")
+    .map((item, index) => {
+      return `<span style="transform:rotate(${index * 8.2}deg)">${item}</span>`;
+    })
+    .join("");
+}
