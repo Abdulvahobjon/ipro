3e7f8582cad9fe51e8b43899202ee15640184965
Abdulvahobjon <abdulvahob0714@gmail.com>
2024-01-04 23:07:58 +0500

    commmid

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,32 @@
-let headerMenuBtn =  document.querySelector("#headerMenuBtn")
-let headerResponsiveMenu =  document.querySelector("#headerResponsiveMenu")
-let headerResponsiveBtn =  document.querySelector("#headerResponsiveBtn")
+let headerMenuBtn = document.querySelector("#headerMenuBtn");
+let headerResponsiveMenu = document.querySelector("#headerResponsiveMenu");
+let headerResponsiveBtn = document.querySelector("#headerResponsiveBtn");
 
-headerMenuBtn.addEventListener('click' , function(){
-  headerResponsiveMenu.classList.add("translate-x-0")
-  headerResponsiveMenu.classList.remove("translate-x-full")
-})
+headerMenuBtn.addEventListener("click", function () {
+  headerResponsiveMenu.classList.add("translate-x-0");
+  headerResponsiveMenu.classList.remove("translate-x-full");
+});
 
+headerResponsiveBtn.addEventListener("click", function () {
+  headerResponsiveMenu.classList.remove("translate-x-0");
+  headerResponsiveMenu.classList.add("translate-x-full");
+});
 
-headerResponsiveBtn.addEventListener('click' , function(){
-  headerResponsiveMenu.classList.remove("translate-x-0")
-  headerResponsiveMenu.classList.add("translate-x-full")
-})
+let section_counter = document.querySelector("#section_counter");
+let counters = document.querySelectorAll(".counter-item .counter");
 
-let section_counter = document.querySelector('#section_counter');
-let counters = document.querySelectorAll('.counter-item .counter');
+document.addEventListener("DOMContentLoaded", function () {
+  var counters = document.querySelectorAll(".counter");
 
-document.addEventListener("DOMContentLoaded", function() {
-  var counters = document.querySelectorAll('.counter');
-  
-  counters.forEach(function(counterElement) {
-    var target = parseInt(counterElement.getAttribute('data-target'));
+  counters.forEach(function (counterElement) {
+    var target = parseInt(counterElement.getAttribute("data-target"));
     var count = 0;
 
-    var updateCounter = function() {
+    var updateCounter = function () {
       var increment = target / 100; // Qancha darajada oshirishni xohlaysiz
       count += increment;
       counterElement.innerText = Math.round(count);
-      
+
       if (count < target) {
         requestAnimationFrame(updateCounter);
       } else {
@@ -36,39 +35,47 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     };
 
-    var handleScroll = function() {
+    var handleScroll = function () {
       var scrollPosition = window.scrollY + window.innerHeight;
       var elementPosition = counterElement.offsetTop;
 
       if (scrollPosition > elementPosition) {
         updateCounter();
-        window.removeEventListener('scroll', handleScroll);
+        window.removeEventListener("scroll", handleScroll);
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
   });
 });
 
 const optionMenu = document.querySelector(".select-menu"),
-    selectBtn = document.querySelector("#select-btn"),
-    options = optionMenu.querySelectorAll(".option");
-    console.log(
-      selectBtn);
+  selectBtn = document.querySelector("#select-btn"),
+  options = optionMenu.querySelectorAll(".option");
 
-selectBtn.addEventListener("click" , function(){
-  optionMenu.classList.toggle("hidden")
-  optionMenu.classList.toggle("flex")
-})
+// Toggle the visibility of optionMenu when selectBtn is clicked
+selectBtn.addEventListener("click", function () {
+  optionMenu.classList.toggle("hidden");
+  optionMenu.classList.toggle("flex");
+});
 
-options.forEach(option => {
-    option.addEventListener("click", () => {
-        let selectedOption = selectBtn.querySelector("span");
-         selectedOption.innerText = option.innerText
+// Add a click event listener to the document to hide the optionMenu when clicking outside of it
+document.addEventListener("click", function (event) {
+  const target = event.target;
+  if (!optionMenu.contains(target) && target !== selectBtn && target !== selectBtn.querySelector('span') && target !== selectBtn.querySelector('svg') && target !== selectBtn.querySelector('path')) {
+    optionMenu.classList.add("hidden");
+    optionMenu.classList.remove("flex");
+  }
+});
 
-        optionMenu.classList.toggle("hidden")
-        optionMenu.classList.toggle("flex")
-    });
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    let selectedOption = selectBtn.querySelector("span");
+    selectedOption.innerText = option.innerText;
+
+    optionMenu.classList.toggle("hidden");
+    optionMenu.classList.toggle("flex");
+  });
 });
 
 function textRounded() {
@@ -80,6 +87,4 @@ function textRounded() {
     })
     .join("");
 }
-textRounded()
-
-
+textRounded();
